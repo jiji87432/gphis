ac27076d7279e9db737f8b675438bbd1bc74e1b5
chenfeng <cf951016@qq.com>
2017-03-02 12:09:31 +0800

    暂时修改

--- idauth/idauth.go
+++ idauth/idauth.go
@@ -1,11 +1,11 @@
-// gphis/idauth - Identity Authentication
-// 身份认证模块
+// gphis/idauth - Identity Authentication 身份认证模块
+//
 package idauth
 
-import (
-	"fmt"
-)
+type IAuth interface {
+	Authenticate() bool
+}
 
-func main() {
-
+func Auth(ia IAuth) bool {
+	return ia.Authenticate()
 }
