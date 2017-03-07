16dc0bcb9fcafcf6b7d861d69f04450c5853981c
chenfeng <cf951016@qq.com>
2017-03-07 18:20:43 +0800

    一些临时的修改

--- idauth/idauth.go
+++ idauth/idauth.go
@@ -2,10 +2,44 @@
 //
 package idauth
 
-type IAuth interface {
-	Authenticate() bool
+import (
+	"fmt"
+)
+
+// 全局登录状态信息存储数据结构
+var tokenList []string
+
+func init() {
+
 }
 
-func Auth(ia IAuth) bool {
+type IAuth interface {
+	Authenticate() (bool, error)
+}
+
+// Auth: 身份验证函数
+func Auth(ia IAuth) (bool, error) {
 	return ia.Authenticate()
+}
+
+// Login: 登录函数
+func Login(ia IAuth) (string, error) {
+
+}
+
+// Logout: 退出函数
+func Logout(ia IAuth) (bool, error) {
+
+}
+
+// Check: 校验token是否合法
+func Check(token string) bool {
+	if len(tokenList) != 0 {
+		for tmp := range tokenList {
+			if tmp == token {
+				return true
+			}
+		}
+	}
+	return false
 }
